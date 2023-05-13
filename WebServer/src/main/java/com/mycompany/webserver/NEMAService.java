5688bbe31abf17abd1c4401342b1440aefbfdec9
TheKraith <127811079+Craig-S-Smith@users.noreply.github.com>
2023-05-13 18:52:50 +1000

    old fires and comments

--- WebServer/src/main/java/com/mycompany/webserver/NEMAService.java
+++ WebServer/src/main/java/com/mycompany/webserver/NEMAService.java
@@ -78,6 +78,7 @@ public class NEMAService {
     @Path("fires")
     @Produces(MediaType.APPLICATION_JSON)
     public String getFires() {
+        // Gets current fires that haven't been deleted
         String result = "";
         Connection connection = null;
         Statement statement = null;
@@ -107,7 +108,7 @@ public class NEMAService {
                 int y_pos = resultSet.getInt("ypos");
                 
                 // Checks if fire is active, if so, adds it to result
-                if (!isActive) {
+                if (isActive) {
                     activeFires = true;
                     // Adds to result string
                     result += "ID: " + id + ", Active: true" + ", Intensity: " + intensity + ", X Position: " + x_pos + ", Y Position: " + y_pos + "\n";
@@ -117,6 +118,76 @@ public class NEMAService {
             // If active fires is still false, sets result to say there are no active fires
             if (!activeFires) {
                 result = "There are no active fires.";
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close all the resources
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        // Returns result
+        return result;
+        
+    }
+    
+    @GET
+    @Path("oldfires")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getOldFires() {
+        // Gets fires that are old and have been deleted
+        String result = "";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        
+        // This boolean will be activated if there is an active fire
+        boolean activeFires = false;
+        
+        try {
+            // Connect to database
+            connection = DriverManager.getConnection(DatabaseURL, DatabaseUser, DatabasePass);
+            
+            // Preparing statement
+            statement = connection.createStatement();
+            
+            // Execute Query
+            String sql = "SELECT * FROM fire";
+            resultSet = statement.executeQuery(sql);
+            
+            while (resultSet.next()) {
+                
+                // Gets details from mySQL
+                int id = resultSet.getInt("id");
+                boolean isActive = resultSet.getBoolean("isActive");
+                int intensity = resultSet.getInt("intensity");
+                int x_pos = resultSet.getInt("xpos");
+                int y_pos = resultSet.getInt("ypos");
+                
+                // Checks if fire is inactive, if so, adds it to result
+                if (!isActive) {
+                    activeFires = true;
+                    // Adds to result string
+                    result += "ID: " + id + ", Active: false" + ", Intensity: " + intensity + ", X Position: " + x_pos + ", Y Position: " + y_pos + "\n";
+                }
+            }
+            
+            // If active fires is still false, sets result to say there are no active fires
+            if (!activeFires) {
+                result = "There are no current fires.";
             }
             
         } catch (SQLException e) {
