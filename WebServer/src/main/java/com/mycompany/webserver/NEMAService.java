382fdb1a30210b3ee9866e18ef9db31d107772e9
TheKraith <127811079+Craig-S-Smith@users.noreply.github.com>
2023-05-13 18:31:09 +1000

    database connect for drone fetch

--- WebServer/src/main/java/com/mycompany/webserver/NEMAService.java
+++ WebServer/src/main/java/com/mycompany/webserver/NEMAService.java
@@ -5,10 +5,37 @@
  */
 package com.mycompany.webserver;
 
+import java.sql.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 /**
  *
  * @author diamo
  */
+
+@Path("service")
 public class NEMAService {
+    // Database connection authentication details
+    static final String DatabaseURL = "jdbc:mysql://localhost:3306/ibdms_server";
+    static final String DatabaseUser = "user";
+    static final String DatabasePass = "pass";
     
+    @GET
+    @Path("drone")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getDrones() {
+        String result = "";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        
+        try {
+            // Connect to database
+            connection = DriverManager.getConnection(DatabaseURL, DatabaseUser, DatabasePass);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
