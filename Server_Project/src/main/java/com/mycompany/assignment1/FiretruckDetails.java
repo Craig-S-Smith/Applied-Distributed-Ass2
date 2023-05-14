862a1a39d068dd053942f0f0814f7ebef096a8c0
CyrilVillanda <129810554+CyrilVillanda@users.noreply.github.com>
2023-05-14 19:14:16 +1000

    added firetruck  class

---
+++ Server_Project/src/main/java/com/mycompany/assignment1/FiretruckDetails.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.assignment1;
+
+/**
+ *
+ * @author cjvil
+ */
+public class FiretruckDetails {
+    
+    int id;
+    String truckName;
+    int fireId;
+
+    public FiretruckDetails() {
+    }
+
+    public FiretruckDetails(int id, String truckName, int fireId) {
+        this.id = id;
+        this.truckName = truckName;
+        this.fireId = fireId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTruckName() {
+        return truckName;
+    }
+
+    public void setTruckName(String truckName) {
+        this.truckName = truckName;
+    }
+
+    public int getFireId() {
+        return fireId;
+    }
+
+    public void setFireId(int fireId) {
+        this.fireId = fireId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("FiretruckDetails{id=").append(id);
+        sb.append(", truckName=").append(truckName);
+        sb.append(", fireId=").append(fireId);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+    
+}
